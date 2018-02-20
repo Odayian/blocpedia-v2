b9d9ec098ef802b921acdc13820ac83c8875aa13
Benjamin Durbin <bwdurbin@gmail.com>
2018-02-20 15:13:41 +0000

    action mailer smtp settings added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,17 +54,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  require 'tlsmail'
-  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-    ActionMailer::Base.delivery_method = :smtp
-     config.action_mailer.perform_deliveries = true
-     config.action_mailer.default :charset => "utf-8"
-       ActionMailer::Base.smtp_settings = {
-       :address              => "smtp.gmail.com",
-       :port                 => 587,
-       :user_name            => "bdurbin.blocpedia@gmail.com",
-       :password             => 'blocpedia',
-       :authentication       => "plain",
-       :enable_starttls_auto => true
-       }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'blocpedia.com',
+  user_name:            'bdurbin.blocpedia',
+  password:             'blocpedia',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 end
